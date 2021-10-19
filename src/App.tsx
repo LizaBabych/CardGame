823d0ff4e42c18b367c1ae8ce5ce94c41537242e
Liza Babych <bettyhetfield11951@gmail.com>
2021-10-19 17:23:30 +0300

    Create card game

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { cards } from "./db";
 
-function App() {
+interface ICard {
+  uid: string;
+  img: string;
+  id: number;
+}
+
+const randomCards = cards.sort(() => Math.random() - 0.5);
+
+const App = () => {
+  const [isAllOpen, setAllOpen] = useState(true);
+  const [disabled, setDisabled] = useState(false);
+
+  const [openedCard, setOpenedCard] = useState<Array<ICard>>([]);
+  const [findCarts, setFindCart] = useState<Array<ICard>>([]);
+
+  const [isGameEnd, setGameEnd] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAllOpen(false);
+    }, 5000);
+  }, []);
+
+  const checkSameCards = (cards: Array<ICard>) => {
+    return cards[0].id === cards[1].id;
+  };
+
+  const openCard = (card: ICard) => {
+    const copy = [...openedCard];
+    copy.push(card);
+    setOpenedCard(copy);
+    if (copy.length === 2) {
+      setDisabled(true);
+      setTimeout(() => {
+        const newFindCart = [...findCarts, ...copy];
+        if (checkSameCards(copy)) {
+          setFindCart(newFindCart);
+        }
+        if (newFindCart.length === randomCards.length) {
+          setGameEnd(true);
+        }
+        setOpenedCard([]);
+        setDisabled(false);
+      }, 500);
+    }
+  };
+
+  const isCardOpen = (card: ICard) => {
+    return (
+      findCarts.find((item) => item === card) ||
+      openedCard.find((item) => item === card)
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Card Game</h1>
+      <div className="cardboard">
+        {randomCards.map((card) => {
+          const isOpen = isCardOpen(card);
+          return (
+            <div
+              className="card"
+              key={card.uid}
+              onClick={() => !isOpen && !disabled && openCard(card)}
+            >
+              {(isOpen || isAllOpen) && (
+                <img src={card.img} alt="card" width="60px" height="60px" />
+              )}
+            </div>
+          );
+        })}
+        {isGameEnd && (
+          <div className="end-game">
+            <h2>You are WIN!!!</h2>
+          </div>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/db.js
@@ -0,0 +1,91 @@
+import one from "./assets/1.png";
+import two from "./assets/2.png";
+import three from "./assets/3.png";
+import four from "./assets/4.png";
+import five from "./assets/5.png";
+import six from "./assets/6.png";
+import seven from "./assets/7.png";
+import eight from "./assets/8.png";
+
+export const cards = [
+  {
+    id: 3842,
+    uid: "10877145-a210-4ecb-9f69-49153257762a",
+    img: one,
+  },
+  {
+    id: 6327,
+    uid: "6b28a95e-1252-40b6-89bd-ef91c4230515",
+    img: two,
+  },
+  {
+    id: 4560,
+    uid: "d944761b-943b-4a85-8174-a0a2e06b0cff",
+    img: three,
+  },
+  {
+    id: 9268,
+    uid: "ed4ee331-6c78-4bb1-ace3-fd316ec388da",
+    img: four,
+  },
+  {
+    id: 3784,
+    uid: "2b5f7e45-7a48-4c4d-b47f-de7b7fa428a0",
+    img: five,
+  },
+  {
+    id: 4372,
+    uid: "a94f4006-2b07-4192-94c0-5fe9ac60f450",
+    img: six,
+  },
+  {
+    id: 5119,
+    uid: "b072a601-8e85-42b9-af96-2924dc71659a",
+    img: seven,
+  },
+  {
+    id: 7013,
+    uid: "8ade9d9a-0313-4753-975c-e77397f25cba",
+    img: eight,
+  },
+  {
+    id: 3842,
+    uid: "1087erferf7145-a210-4ecb-9f69-49153257762a",
+    img: one,
+  },
+  {
+    id: 6327,
+    uid: "6b28a95eerf-1252-40b6-89bd-ef91c4230515",
+    img: two,
+  },
+  {
+    id: 4560,
+    uid: "d9447wdwed61b-943b-4a85-8174-a0a2e06b0cff",
+    img: three,
+  },
+  {
+    id: 9268,
+    uid: "ed4eewedwe331-6c78-4bb1-ace3-fd316ec388da",
+    img: four,
+  },
+  {
+    id: 3784,
+    uid: "2b5dwedf7e45-7a48-4c4d-b47f-de7b7fa428a0",
+    img: five,
+  },
+  {
+    id: 4372,
+    uid: "a9ded4f4006-2b07-4192-94c0-5fe9ac60f450",
+    img: six,
+  },
+  {
+    id: 5119,
+    uid: "b07de2a601-8e85-42b9-af96-2924dc71659a",
+    img: seven,
+  },
+  {
+    id: 7013,
+    uid: "8deade9d9a-0313-4753-975c-e77397f25cba",
+    img: eight,
+  },
+];
